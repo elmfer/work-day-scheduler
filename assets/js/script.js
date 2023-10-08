a106968de083f76bab75276303b1a719e425bd09
elmfer <elmfer11@outlook.com>
2023-10-08 00:39:27 -0500

    Added comments to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,71 @@
+// All available work hours
 const hours = [
     '9AM', '10AM', '11AM', '12PM', '1PM',
     '2PM', '3PM', '4PM', '5PM'
 ];
 
+// Same as the above but in 24 hour format
 const hours24Format = [
     '09:00', '10:00', '11:00', '12:00', '13:00',
     '14:00', '15:00', '16:00', '17:00'
 ];
 
+// Determine wether the selected hour in the past, present, or future
 function isPastFutureOrPresent(hourIndex) {
     var now = dayjs(dayjs().format('YYYY-MM-DD HH:00'));
-    //var now = dayjs('2023-10-07 11:00');
-    //now = dayjs(now.format('YYYY-MM-DD HH:00'));
 
     var hour24Format = hours24Format[hourIndex];
     var currentHour = dayjs(dayjs().format('YYYY-MM-DD ' + hour24Format));
     
+    // A negative difference in time from the selected hour to now is in the past
     if(currentHour.diff(now, 'hour') < 0) {
         return 'past';
     }
 
+    // No difference in time from the selected hour to now is in the present
     if(currentHour.diff(now, 'hour') == 0) {
         return 'present';
     }
 
+    // Otherwise, it must be in the future
     return 'future';
 }
 
+// Called when the save button on its respective hour block is pressed
+// Saves the agenda for that hour in local storage
 function onSaveSchedule(event) {
     var btn = $(event.target);
 
+    // Grab the value from the respective text area of that hour block
     const agenda = $('#hour-' + btn.data('hour')).val();
 
+    // Save it in local storage
     localStorage.setItem(btn.data('hour'), agenda);
 }
 
+// Load schedule from local storage upon loading the app
 function loadSchedule() {
+    // Grab the current date
     var today = dayjs(dayjs().format('YYYY-MM-DD'));
 
+    // Grab the last day this app was used
     var lastDayUsed = localStorage.getItem('last-time-used');
-
     if(lastDayUsed === null) {
-        localStorage.setItem('last-time-used', today.format('YYYY-MM-DD'));
+        const lastTimeFormatted = today.format('YYYY-MM-DD');
+        localStorage.setItem('last-time-used', lastTimeFormatted);
+        lastDayUsed = lastTimeFormatted;
     }
 
     lastDayUsed = dayjs(lastDayUsed);
 
+    // If this app is used in a new day, clear the schedule and note the current day
     if(today.diff(lastDayUsed, 'day') > 0) {
         localStorage.clear();
         localStorage.setItem('last-time-used', today.format('YYYY-MM-DD'));
         return;
     }
 
+    // Otherwise, load schedule onto the hour blocks
     for(var i = 0; i < hours.length; i++) {
         const agenda = localStorage.getItem(hours[i]);
 
@@ -61,12 +75,16 @@ function loadSchedule() {
     }
 }
 
+// Generate all the hour blocks
 function loadWorkHours() {
     var scheduleContainer = $('#schedule');
     
+    // Loop through all available hours
     for(var i = 0; i < hours.length; i++) {
+        // Determine if the hour is in the past, present, or future
         var status = isPastFutureOrPresent(hours[i]);
 
+        // Create the hour block
         var hourContainer = $('<div>');
         hourContainer.addClass('row time-block ' + isPastFutureOrPresent(i));
 
@@ -89,34 +107,20 @@ function loadWorkHours() {
         saveBtn.on('click', onSaveSchedule);
         hourContainer.append(saveBtn);
 
+        // Append the hour block to the schedule
         scheduleContainer.append(hourContainer);
     }
 }
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Called upon the document fully loading
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
+    // Generate hour blocks
     loadWorkHours();
+
+    // Load existing schedule
     loadSchedule();
 
-    // TODO: Add code to display the current date in the header of the page.
+    // Display the current day on top
     var currentDate = dayjs().format('dddd, MMMM D');
     $('#currentDay').text(currentDate);
   });
